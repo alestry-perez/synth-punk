bb56dbea440a2e836a9ed70ca10245ac45da1800
Alestry Pérez <alestry.perez@hyperisland.se>
2020-11-05 23:25:06 +0100

    tone js working

--- dist/toneAudio.6d799d00.js
+++ dist/toneAudio.6d799d00.js
@@ -50753,29 +50753,11 @@ function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return 
 
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 
-function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
-
-function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
-
-//attach a click listener to a play button
-document.getElementById("play-button").addEventListener("click", /*#__PURE__*/_asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee() {
-  return regeneratorRuntime.wrap(function _callee$(_context) {
-    while (1) {
-      switch (_context.prev = _context.next) {
-        case 0:
-          _context.next = 2;
-          return Tone.start();
-
-        case 2:
-          console.log("audio is ready");
-
-        case 3:
-        case "end":
-          return _context.stop();
-      }
-    }
-  }, _callee);
-})));
+// //attach a click listener to a play button
+// document.getElementById("play-button").addEventListener("click", async () => {
+//   await Tone.start();
+//   console.log("audio is ready");
+// });
 var synth = new Tone.Synth().toDestination();
 var now = Tone.now();
 synth.triggerAttackRelease("C4", "8n", now);
@@ -50809,7 +50791,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49674" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54474" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- toneAudio.js
+++ toneAudio.js
@@ -1,10 +1,10 @@
 import * as Tone from "tone";
 
-//attach a click listener to a play button
-document.getElementById("play-button").addEventListener("click", async () => {
-  await Tone.start();
-  console.log("audio is ready");
-});
+// //attach a click listener to a play button
+// document.getElementById("play-button").addEventListener("click", async () => {
+//   await Tone.start();
+//   console.log("audio is ready");
+// });
 
 const synth = new Tone.Synth().toDestination();
 const now = Tone.now();
