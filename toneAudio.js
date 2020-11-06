f3011411f28a59c8f7b6ab4e1032cef294d90cbb
Alestry Pérez <alestry.perez@hyperisland.se>
2020-11-06 15:59:57 +0100

    commit

--- toneAudio.js
+++ toneAudio.js
@@ -8,6 +8,7 @@ import * as Tone from "tone";
 
 const synth = new Tone.Synth().toDestination();
 const now = Tone.now();
+
 synth.triggerAttackRelease("C4", "8n", now);
 synth.triggerAttackRelease("E4", "8n", now + 0.5);
 synth.triggerAttackRelease("G4", "8n", now + 1);
