d6541afa6801626c1105c0a60da18e4ee7b0b5e3
Alestry Pérez <alestry.perez@hyperisland.se>
2021-03-23 18:42:45 +0100

    added p5
    testing to determine whether it would be a good addition to the project.

--- dist/audioTone.c828ecd0.js
+++ dist/audioTone.c828ecd0.js
@@ -51497,10 +51497,116 @@ function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return 
 
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 
-//create a synth and connect it to the main output (your speakers)
-var synth = new Tone.Synth().toDestination(); //play a middle 'C' for the duration of an 8th note
+var masterVolume = -9; // in decibel.
 
-synth.triggerAttackRelease('C4', '8n');
+var ready = false;
+var wave; // this object allows us to draw waveforms
+
+var synth;
+var loop; //let scale = ['C4', 'D4','E4', 'F4', 'G4', 'A4', 'B4']; // C major
+
+var scale;
+var prevNote; //------------------------------------------------------------
+// Create a new canvas to match the browser size
+
+function setup() {
+  createCanvas(windowWidth, windowHeight); // Look at Tonal.ScaleType.names() for a full list of
+  // supported scales
+
+  scale = Tonal.Scale.get('C4 minor pentatonic').notes;
+} //------------------------------------------------------------
+
+
+function initalizeAudio() {
+  // Create the Synth
+  synth = new Tone.Synth();
+  synth.toDestination(); // synth.connect(Tone.Master);
+  // note that the first parameter to Loop() is an anonymous function,
+  // ie a function defined "on the fly"
+
+  loop = new Tone.Loop(function (time) {
+    var n = noise(frameCount * 0.1);
+    var i = floor(map(n, 0, 1, 0, scale.length)); // floor rounds down
+
+    var note = scale[i];
+
+    if (prevNote != note) {
+      // (freq, noteDuration, time)
+      synth.triggerAttackRelease(note, '8n', time);
+    }
+
+    prevNote = note;
+  }, '16n'); // '16n' here sets the speed of our loop -- every 16th note
+  // Start the loop
+
+  loop.start();
+  wave = new Tone.Waveform();
+  Tone.Master.connect(wave);
+  Tone.Master.volume.value = masterVolume;
+  Tone.Transport.start();
+} //------------------------------------------------------------
+// On window resize, update the canvas size
+
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+} //------------------------------------------------------------
+// Main render loop
+
+
+function draw() {
+  background(0);
+
+  if (ready) {
+    drawWaveform(wave);
+  } else {
+    fill(255);
+    noStroke();
+    textAlign(CENTER, CENTER);
+    text('CLICK TO START', width / 2, height / 2);
+  }
+} //------------------------------------------------------------
+
+
+function drawWaveform(wave) {
+  var w = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : width;
+  var h = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : height;
+  stroke(255);
+  var buffer = wave.getValue(0); // look a trigger point where the samples are going from
+  // negative to positive
+
+  var start = 0;
+
+  for (var i = 1; i < buffer.length; i++) {
+    if (buffer[i - 1] < 0 && buffer[i] >= 0) {
+      start = i;
+      break; // interrupts a for loop
+    }
+  } // calculate a new end point such that we always
+  // draw the same number of samples in each frame
+
+
+  var end = start + buffer.length / 2; // drawing the waveform
+
+  for (var _i = start; _i < end; _i++) {
+    var x1 = map(_i - 1, start, end, 0, w);
+    var y1 = map(buffer[_i - 1], -1, 1, 0, h);
+    var x2 = map(_i, start, end, 0, w);
+    var y2 = map(buffer[_i], -1, 1, 0, h);
+    line(x1, y1, x2, y2);
+  }
+} //------------------------------------------------------------
+
+
+function mousePressed() {
+  if (!ready) {
+    ready = true;
+    initalizeAudio();
+  } else {
+    ready = false;
+    Tone.Transport.stop();
+  }
+}
 },{"tone":"node_modules/tone/build/esm/index.js"}],"node_modules/parcel/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
