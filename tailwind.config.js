b2d76d76be30cf8dda59ac414db6aff49fe204f4
Alestry Pérez <alestry.perez@hyperisland.se>
2021-03-23 14:45:42 +0100

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,39 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        completed: "#83B919",
+        success: "#587e0e",
+        'harvest-gold': {
+          50: '#fdfbf8',
+          100: '#faf7f1',
+          200: '#f4ebdb',
+          300: '#eddfc5',
+          400: '#dfc79a',
+          500: '#d1af6f',
+          600: '#bc9e64',
+          700: '#9d8353',
+          800: '#7d6943',
+          900: '#665636',
+        },
+        'chambray': {
+          50: '#f5f6f9',
+          100: '#eaeef4',
+          200: '#cbd3e3',
+          300: '#abb9d3',
+          400: '#6d85b1',
+          500: '#2e5090',
+          600: '#294882',
+          700: '#233c6c',
+          800: '#1c3056',
+          900: '#172747',
+        },
+      }
+    },
   },
-  variants: {},
+  variants: {
+    backgroundColor: ['responsive', 'hover', 'focus', 'active'],
+  },
   plugins: [],
 };
