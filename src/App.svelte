0312c377d3af6f9f672d2f75778676e12cec5b05
Alestry Pérez <alestry.perez@hyperisland.se>
2021-09-09 18:42:53 -0400

    fixed svelte layout

--- src/App.svelte
+++ src/App.svelte
@@ -1,67 +1,67 @@
 <script>
-  import logo from './assets/svelte.png'
-  import Counter from './lib/Counter.svelte'
+  import AmpEnv from './lib/AmpEnv.svelte';
+import Efx from './lib/Efx.svelte';
+import OscKnobOne from './lib/OscKnobOne.svelte';
+import OscKnobTwo from './lib/OscKnobTwo.svelte';
+  import PlayButton from './lib/PlayButton.svelte';
+  import WaveDisplayOne from './lib/WaveDisplayOne.svelte';
+import WaveDisplayTwo from './lib/WaveDisplayTwo.svelte';
+  import WaveSelectOne from './lib/WaveSelectOne.svelte';
+import WaveSelectTwo from './lib/WaveSelectTwo.svelte';
 </script>
 
 <main>
-  <img src={logo} alt="Svelte Logo" />
-  <h1>Hello world!</h1>
+  <body>
+    <div class="grid h-screen place-items-center">
+      <div>
+        <h1 class="text-2xl font-black">シンセパンク</h1>
+      </div>
+      <div
+        id="synth"
+        class="grid grid-cols-3 grid-rows-1 gap-0 border-2 border-black divide-x-2 divide-black  bg-primary-500 border-box"
+      >
+        <div id="oscGroup" class="grid grid-flow-col grid-rows-3">
+          <!-- OSC 1 -->
+          <div
+            id="oscOne"
+            class="grid grid-flow-col grid-rows-4 gap-0 place-content-evenly"
+          >
+            <h2 class="self-center font-bold text-center underline uppercase">
+              osc - 1
+            </h2>
+            <!-- Waveform Display -->
+            <WaveDisplayOne/>
+            <!-- Waveform Select -->
+            <WaveSelectOne/>
+            <!-- Knobs -->
+            <OscKnobOne/>
+          </div>
 
-  <Counter />
-
-  <p>
-    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
-    apps.
-  </p>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
-    the officially supported framework, also powered by Vite!
-  </p>
+          <!-- OSC 2 -->
+          <div
+            id="oscTwo"
+            class="grid grid-flow-col grid-rows-4 row-start-2 gap-0  place-content-evenly"
+          >
+            <h2 class="self-center font-bold text-center underline uppercase">
+              osc - 2
+            </h2>
+            <!-- Waveform Display -->
+            <WaveDisplayTwo/>
+            <!-- Waveform Select -->
+            <WaveSelectTwo/>
+            <!-- Knobs -->
+            <OscKnobTwo/>
+          </div>
+          <!-- Play-Stop Button -->
+        <PlayButton />
+        </div>
+        <AmpEnv/>
+        <Efx/>
+      </div>
+    </div>
+  </body>
 </main>
 
 <style>
 
-  main {
-    @apply 
-    flex 
-    flex-col 
-    text-center 
-    p-1 
-    m-0 
-    items-center 
-    justify-center;
-  }
-
-  img {
-    @apply 
-    h-20 
-    w-20;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4rem;
-    font-weight: 100;
-    line-height: 1.1;
-    margin: 2rem auto;
-    max-width: 14rem;
-  }
-
-  p {
-    max-width: 14rem;
-    margin: 1rem auto;
-    line-height: 1.35;
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      max-width: none;
-    }
-
-    p {
-      max-width: none;
-    }
-  }
 </style>
